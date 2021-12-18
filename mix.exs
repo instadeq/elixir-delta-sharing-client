f1c2e50a01312119f5a03676766872806e4e0d85
Mariano Guerra <mariano@marianoguerra.org>
2021-12-18 19:58:13 -0300

    new release

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule DeltaSharing.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/instadeq/elixir-delta-sharing-client"
-  @version "0.1.0"
+  @version "0.2.0"
 
   def project do
     [
